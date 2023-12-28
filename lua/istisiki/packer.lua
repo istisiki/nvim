7524a6de5d2a1dff452266bef5d2f9c28b6d26bb
Stacey Cardoso <1961027+istisiki@users.noreply.github.com>
2023-12-28 02:16:06 +0000

    remove whichkey and neotree;

--- lua/istisiki/packer.lua
+++ lua/istisiki/packer.lua
@@ -43,27 +43,4 @@ return require('packer').startup(function(use)
 	}
 
 	use('nvim-tree/nvim-web-devicons')
-	use {
-    "folke/which-key.nvim",
-    config = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-      require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  }
-
-	use {
-  "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    requires = { 
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  }
 end)
